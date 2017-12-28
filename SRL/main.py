bf66f57d0a63c315fe30ed494c25091da0b235a4
Shi Peng <spone.2014@gmail.com>
2017-12-28 01:04:22 -0500

    SRL model first commit, under testing

---
+++ SRL/main.py
@@ -0,0 +1,165 @@
+import torch
+from torchtext import data
+import numpy as np
+from pbase import app, logger
+from model import SRL
+
+WORD = data.Field(batch_first=True, lower=True)
+PLEMMA = data.Field(batch_first=True)
+PPOS = data.Field(batch_first=True)
+DEP = data.Field(batch_first=True, use_vocab=False, preprocessing=lambda x: [int(y) for y in x], pad_token=-1)
+LABEL = data.Field(batch_first=True)
+INDICATOR = data.Field(batch_first=True)
+SLABEL = data.Field(batch_first=True)
+fields = [('WORD', WORD), ('PLEMMA', PLEMMA), ('PPOS', PPOS), ('DEP', DEP), ('LABEL', LABEL),
+          ('INDICATOR', INDICATOR), ('SLABEL', SLABEL)]
+include_test = [False, False, False, False, False, False, False]
+params = [{"min_freq":2}, {}, {}, {}, {}, {}, {}]
+
+class Args(app.ArgParser):
+    def __init__(self):
+        super(Args, self).__init__(description="SRL", patience=10000)
+        self.parser.add_argument('--word_dim', type=int, default=100)
+        self.parser.add_argument('--pos_dim', type=int, default=100)
+        self.parser.add_argument('--lem_dim', type=int, default=100)
+        self.parser.add_argument('--srl_dim', type=int, default=100)
+        self.parser.add_argument('--is_verb_dim', type=int, default=1)
+        self.parser.add_argument('--lstm_hidden', type=int, default=400)
+        self.parser.add_argument('--num_lstm_layer', type=int, default=3)
+        self.parser.add_argument('--lstm_dropout', type=float, default=0.33)
+        self.parser.add_argument('--vector_cache', type=str,
+                                 default="/mnt/collections/p8shi/dev/biaffine/Biaffine/data/glove.100d.conll09.pt")
+        self.parser.add_argument('--lr', type=float, default='1e-3')
+        self.parser.add_argument('--tensorboard', type=str, default='logs')
+
+class Trainer(app.TrainAPP):
+    def __init__(self, **kwargs):
+        super(Trainer, self).__init__(**kwargs)
+        self.config.word_num = len(self.WORD.vocab)
+        self.config.pos_num = len(self.PPOS.vocab)
+        self.config.lem_num = len(self.PLEMMA.vocab)
+        self.config.is_verb_num = len(self.INDICATOR.vocab)
+        self.config.srl_num = len(self.SLABEL.vocab)
+        if self.config.cuda:
+            self.config.PAD = WORD.vocab.stoi['<pad>']
+            self.config.ISVERB = INDICATOR.vocab.stoi['1']
+        stoi, vectors, dim = torch.load(self.config.vector_cache)
+        match_embedding = 0
+        self.WORD.vocab.vectors = torch.Tensor(len(self.WORD.vocab), dim)
+        for i, token in enumerate(self.WORD.vocab.itos):
+            wv_index = stoi.get(token, None)
+            if wv_index is not None:
+                self.WORD.vocab.vectors[i] = vectors[wv_index]
+                match_embedding += 1
+            else:
+                self.WORD.vocab.vectors[i] = torch.FloatTensor(self.config.word_dim).uniform_(-0.05, 0.05)
+        print("Matching {} out of {}".format(match_embedding, len(self.WORD.vocab)))
+
+    def prepare(self, **kwargs):
+        super(Trainer, self).prepare(**kwargs)
+        self.model.pre_embed.weight.data.copy_(self.WORD.vocab.vectors)
+        print(self.model)
+        print(self.SLABEL.vocab.itos)
+        print(self.config)
+
+class optimizer:
+    def __init__(self, parameter, config):
+        self.optim = torch.optim.Adam(parameter, lr=config.lr, betas=(0.9, 0.9), eps=1e-12)
+        l = lambda epoch: 0.75 ** (epoch // 8)
+        self.scheduler = torch.optim.lr_scheduler.LambdaLR(self.optim, lr_lambda=l)
+
+    def zero_grad(self):
+        self.optim.zero_grad()
+
+    def step(self):
+        self.optim.step()
+
+    def schedule(self):
+        self.scheduler.step()
+        print("learning rate : ", self.scheduler.get_lr(), self.scheduler.base_lrs)
+
+
+class criterion:
+    def __init__(self):
+        self.crit = torch.nn.CrossEntropyLoss(ignore_index=-1)
+
+    def __call__(self, output, batch):
+        return self.crit(output.view(-1, output.size(2)), batch.SLABEL.view(-1, 1)[:,0])
+
+def evaluator(name, pairs):
+    # pair = (batch_output, batch_example)
+    if type(pairs) != list and type(pairs) == tuple:
+        pairs = [pairs]
+    n_correct = 0
+    n_total = 0
+    n_predicted = 0
+    acc_total = 0
+    acc_right = 0
+    for (output, batch) in pairs:
+        acc_right += (torch.max(output, 2)[1].view(batch.SLABEL.size()).data == batch.SLABEL.data).sum(dim=1).sum()
+        size_list = list(batch.SLABEL.size())
+        acc_total += size_list[0] * size_list[1]
+        pred = torch.max(output, 2)[1].view(batch.SLABEL.size()).cpu().data.numpy()
+        gold = batch.SLABEL.cpu().data.numpy()
+        for pred_sent, gold_sent in zip(pred, gold):
+            for pred_token, gold_token in zip(pred_sent, gold_sent):
+                if gold_token == SLABEL.vocab.stoi['<pad>']:
+                    continue
+                if pred_token == gold_token and gold_token != SLABEL.vocab.stoi['_']:
+                    n_correct += 1
+                if pred_token != SLABEL.vocab.stoi['_']:
+                    n_predicted += 1
+                if gold_token != SLABEL.vocab.stoi['_']:
+                    n_total += 1
+    if n_predicted == 0:
+        precision = 0
+    else:
+        precision = n_correct / n_predicted
+    if n_total == 0:
+        recall = 0
+    else:
+        recall = n_correct / n_total
+    if precision + recall == 0:
+        f1 = 0
+    else:
+        f1 = 2 * precision * recall / (precision + recall)
+    if acc_total == 0:
+        acc = 0
+    else:
+        acc = acc_right / acc_total
+    return acc, precision, recall, f1
+
+
+
+def metrics_comparison(new_metrics, best_metrics):
+    if best_metrics == None or new_metrics[3] > best_metrics[3]:
+        return True
+    return False
+
+def log_printer(name, metrics, loss, epoch=None, iters=None):
+    if name == 'train':
+        print("{}\tEPOCH : {}\tITER : {}\tACC : {}\tP : {}\tR : {}\tF : {}\tNearest batch training LOSS : {}".format(
+            name, epoch, iters, metrics[0], metrics[1], metrics[2], metrics[3], loss
+        ))
+        step = int(iters.split('/')[0]) + int(iters.split('/')[1]) * (epoch-1)
+        log.scalar_summary(tag="loss", value=loss, step=step)
+    else:
+        if loss == None:
+            print("{}\tACC : {}\tP : {}\tR : {}\tF : {}".format(name, metrics[0], metrics[1], metrics[2], metrics[3]))
+        else:
+            print("{}\tACC : {}\tP : {}\tR : {}\tF : {}".format(name, metrics[0], metrics[1], metrics[2], metrics[3], loss))
+        if iters != None and epoch != None and loss != None:
+            step = int(iters.split('/')[0]) + int(iters.split('/')[1]) * (epoch - 1)
+            log.scalar_summary(tag="valid_loss", value=loss, step=step)
+
+if __name__=='__main__':
+    arg_parser = Args()
+    args = arg_parser.get_args()
+    log = logger.Logger(args.tensorboard)
+    crit = criterion()
+    trainer = Trainer(args=args, fields=fields, include_test=include_test,
+                      build_vocab_params=params)
+    trainer.prepare(model=SRL, optimizer=optimizer, criterion=crit,
+                    evaluator=evaluator, metrics_comparison=metrics_comparison,
+                    log_printer=log_printer)
+    trainer.train()
--- SRL/model.py
+++ SRL/model.py
@@ -3,7 +3,7 @@ from torch import nn
 from pbase import layer
 
 
-class SRL:
+class SRL(nn.Module):
     def __init__(self, config):
         super(SRL, self).__init__()
         self.config = config
@@ -12,6 +12,7 @@ class SRL:
         self.pre_embed.weight.requires_grad = False
         self.pos_embed = nn.Embedding(config.pos_num, config.pos_dim)
         self.lem_embed = nn.Embedding(config.lem_num, config.lem_dim)
+        self.srl_embed = nn.Embedding(config.srl_num, config.srl_dim)
         self.is_verb_embed = nn.Embedding(config.is_verb_num, config.is_verb_dim)
         self.embed_dropout = nn.Dropout(p=0.33)
         self.lstm = layer.LSTM(
@@ -22,6 +23,14 @@ class SRL:
             batch_first=True,
             bidirectional=True
         )
+        if config.cuda:
+            self.srl_range = torch.autograd.Variable(torch.LongTensor(list(range(self.config.srl_num)))).cuda(config.gpu)
+        else:
+            self.srl_range = torch.autograd.Variable(torch.LongTensor(list(range(self.config.srl_num))))
+        self.SRLParaMLP = layer.MLP(in_features=config.srl_dim+config.lem_dim,
+                                    out_features=config.lstm_hidden * 4,
+                                    activation=nn.LeakyReLU(0.1),
+                                    dropout=0.33)
 
 
     def forward(self, x):
@@ -30,14 +39,45 @@ class SRL:
         x_pre_embed = self.embed_dropout(self.pre_embed(x.WORD))
         x_embed = x_word_embed + x_pre_embed
         x_pos_embed = self.embed_dropout(self.pos_embed(x.PPOS))
-        x_lem_embed = self.embed_dropout(self.lem_embed(x.LEMMA))
-        x_is_verb_embed = self.is_verb_embed(x.ISVERB)
+        x_lem_embed = self.embed_dropout(self.lem_embed(x.PLEMMA))
+        x_is_verb_embed = self.is_verb_embed(x.INDICATOR)
         x_lexical = torch.cat((x_embed, x_pos_embed, x_lem_embed, x_is_verb_embed), dim=2)
         outputs, (ht, ct) = self.lstm(x_lexical)
         # output = (batch_size, sentence_length, hidden_size * num_direction)
         # Pass the is_verb == 1 through config
-        predicate_index = torch.equal(x.ISVERB, self.config.ISVERB)
-        predicate_hidden = torch.sum(outputs * predicate_index, dim=1)
+        predicate_index = (torch.eq(x.INDICATOR, self.config.ISVERB)).float()
+        predicate_hidden = torch.sum(outputs * predicate_index.unsqueeze(dim=2), dim=1).unsqueeze(dim=1)
+        # predicate_hidden = (batch_size, 1, hidden_size * num_direction)
+        token_to_keep = (1 - torch.eq(x.WORD, self.config.PAD).float()).unsqueeze(dim=2)
+        # token_to_keep = (batch_size, sentence_length, 1)
+        predicate_broadcast = token_to_keep * predicate_hidden
+        mlp_input = torch.cat([outputs, predicate_broadcast], dim=2)
+        # mlp_input = (batch_size, sentence_length, lstm_hidden * 4)
+
+        # Generate parameters for SRL
+        batch_size = x.WORD.size(0)
+        role_embed = self.srl_embed(self.srl_range)
+        role_embed = torch.cat([role_embed.unsqueeze(dim=0)] * batch_size, dim=0)
+        # role_embed = (srl_num, srl_dim)
+        # print(predicate_index)
+        # print(x.WORD)
+        # print((predicate_index * x.WORD.float()).size())
+        predicate_word_id = torch.sum(predicate_index * x.PLEMMA.float(), dim=1).long()
+        predicate_lem = self.embed_dropout(self.lem_embed(predicate_word_id))
+        predicate_lem = torch.cat([predicate_lem.unsqueeze(dim=1)] * self.config.srl_num, dim=1)
+        pre_para = torch.cat([role_embed, predicate_lem], dim=2)
+        # pre_para = (batch_size, srl_num, pre_para_dim)
+        srl_para = self.SRLParaMLP(pre_para).transpose(1, 2)
+        # srl_para = (batch_size, srl_num, lstm_hidden * 4)
+        logit = torch.matmul(mlp_input, srl_para)
+        # logit = (batch_size, sentence_length, srl_num)
+        return logit
+
+
+
+
+
+
         
 
 
